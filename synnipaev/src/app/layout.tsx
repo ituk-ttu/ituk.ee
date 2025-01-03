bea7f628c5fed174415e123758163d04270f31a4
krpode <krpode@taltech.ee>
2025-01-03 21:38:11 +0200

    Basic routes, header and footer

--- synnipaev/src/app/layout.tsx
+++ synnipaev/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
+
         {children}
+
+        <Footer />
       </body>
     </html>
   );
