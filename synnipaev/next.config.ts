64e7a977eb76fb8ae67e1a250daf4af977f46bd8
krpode <krpode@taltech.ee>
2025-01-15 11:37:04 +0200

    fixed .env

--- synnipaev/next.config.ts
+++ synnipaev/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+require("dotenv").config();
+
 const nextConfig: NextConfig = {
-  
+  env: {
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+    APP_ID: process.env.APP_ID,
+    MEASUREMENT_ID: process.env.MEASUREMENT_ID
+  }
 };
 
 export default nextConfig;
--- synnipaev/src/firebase.tsx
+++ synnipaev/src/firebase.tsx
@@ -3,9 +3,9 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSrC4I459UAlcx06xzkRT68OCO_6HIqlE",
+  apiKey: process.env.API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: "ituk-ee",
+  projectId: process.env.PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
