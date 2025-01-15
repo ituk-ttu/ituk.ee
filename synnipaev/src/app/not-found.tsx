250a92416f9de6b205eaeff007b98c119d4b3fa3
krpode <krpode@taltech.ee>
2025-01-15 14:03:07 +0200

    not-found redirect

--- synnipaev/src/app/not-found.tsx
+++ synnipaev/src/app/not-found.tsx
@@ -1,15 +1,6 @@
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
  
 export default function NotFound() {
-  return (
-    <html>
-      <body>
-        <div>
-          <h2>404</h2>
-          <p>Could not find requested resource</p>
-          <Link href="/">Return Home</Link>
-        </div>
-      </body>
-    </html>
-  )
+  redirect('/not-found');
 }
